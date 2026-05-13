f8d0ed20f78033ff3d76a0ef0f15fb9445b31e82
mohd ayan raza <ayan@ayans-macbook.local>
2026-05-13 12:20:48 +0530

    multiple workflow added

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,3 +1,5 @@
+# Running a series of shell commands directly in a workflow can quickly become messy. By placing your commands in a standalone script file and invoking it in one step, you maintain a clean, maintainable workflow.
+
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON...RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
